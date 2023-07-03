32ef21c2d6a0172d402925fb0030931ec5360817
William S <will.step.205@gmail.com>
2023-07-03 16:30:45 -0700

    writing to FirebaseDB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const your_url = "https://my-react-project-9ba22-default-rtdb.firebaseio.com/"
+
+const appSettings = { databaseURL: your_url }
+
+const app = initializeApp(appSettings)
+const db = getDatabase(app)
+const dbRef = ref(db, "products")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+
+    push(dbRef, inputValue)
+
+    console.log(`${inputValue} was added to the database!`)
 })
